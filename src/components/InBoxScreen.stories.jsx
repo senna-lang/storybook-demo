e244fcc240ab8b868cc9bc924c1f18cc6857bb82
senna-lang <asrsena3302@gmail.com>
2024-02-03 15:42:16 +0900

    Error story 追加

--- src/components/InBoxScreen.stories.jsx
+++ src/components/InBoxScreen.stories.jsx
@@ -3,6 +3,7 @@ import store from '../lib/store';
 import { rest } from 'msw';
 import { MockedState } from './TaskList.stories';
 import InBoxScreen from './inBoxScreen';
+import { fireEvent, waitFor, waitForElementToBeRemoved, within } from '@storybook/test';
 
 export default {
   component: InBoxScreen,
@@ -12,14 +13,40 @@ export default {
 };
 
 export const Default = {
-   parameters: {
-      msw: {
-         handlers: [
-            rest.get('https://jsonplaceholder.typicode.com/todos?userId=1',
-            (req,res,ctx) => {
-               return res(ctx.json(MockedState.tasks));
-            })
-         ]
-      }
-   }
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(
+          'https://jsonplaceholder.typicode.com/todos?userId=1',
+          (req, res, ctx) => {
+            return res(ctx.json(MockedState.tasks));
+          }
+        ),
+      ],
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await waitForElementToBeRemoved(await canvas.findAllByTestId('loading'));
+    await waitFor(async() => {
+      await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+      await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+      await fireEvent.click(canvas.getByLabelText('archiveTask-5'));
+    })
+  },
+};
+
+export const Error = {
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(
+          'https://jsonplaceholder.typicode.com/todos?userId=1',
+          (req, res, ctx) => {
+            return res(ctx.status(403));
+          }
+        ),
+      ],
+    },
+  },
 };
